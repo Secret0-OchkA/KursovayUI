3cac3e91799232d013000ed44cdf86a7faeb8ce3
Secre0_OchkA <scretochka@yandex.ru>
2022-11-19 19:01:15 +0000

    add docker

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get("/", function (req, res) {
   res.sendFile("/public/html/index.html", { root: __dirname });
 });
-app.get("/test", function (req, res) {
-  res.sendFile("/public/html/test.html", { root: __dirname });
-});
 
 app.listen(3000);
+console.log("run node server");
